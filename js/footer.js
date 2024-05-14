5698a44261938a65b60551dc3df841dc97dc21b9
lglopez <lglopez@scl.edu.gt>
2024-05-14 08:48:37 -0600

    version 2.0 header y footer gabriel

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,1 @@
-let footer=document.querySelector(".footer")
-    footer.innerHTML=`
-    <h2>CORREOS</h2>
-        <div class="creadores">
-            <p class="cr-1">adrodas@scl.edu.gt</p>
-            <p class="cr-2">lglopez@scl.edu.gt</p>
-        </div>
-    `
+a
--- js/header.js
+++ js/header.js
@@ -1,16 +1,1 @@
-let li_header=document.querySelector(".header")
-li_header.innerHTML=`
-    <div class="logo">
-        <div class="l-img">
-            <img src="img/roar.jpg"  alt="">
-        </div>
-        <h2 class="ltex">@adrodas</h2>
-    </div>
-    <h1>COMPONENTES DEL COMPUTADOR</h1>
-    <div class="logo-2">
-     <div class="l-img-2">
-     <img src="img/ranita.jpg"  alt="">
-     </div>
-     <h2 class="ltex-2">@lglopez</h2>
-    </div>
-`;
+a
