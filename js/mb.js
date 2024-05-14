2b824319d3dce11bac090736800e5961218c1c42
adrodas <adrodas@scl.edu.gt>
2024-05-14 11:22:44 -0600

    version_2.0 footer and css

--- js/mb.js
+++ js/mb.js
@@ -1,34 +1,5 @@
 let li_section=document.querySelector(".inf")
     li_section.innerHTML=`
-    <div class="inf-mn">
-        <nav class="menu">
-            <li class="menu-opt-1"><a href="index.html">Index</a></li>
-            <li class="menu-opt-2"><a href="cpu.html">CPU</a></li>
-            <li class="menu-opt-3"><a href="gpu.html">GPU</a></li>
-            <li class="menu-opt-4"><a href="mb.html">MB</a></li>
-            <li class="menu-opt-5"><a href="ram.html">RAM</a></li>
-        </nav>
-    </div>
-    <div class="inf-des">
-        <h1 class="des-tl"> MB</h1>
-        <div class="des-tx">
-            <p> La MB, o Placa Base, es el componente central de un ordenador que facilita la comunicación entre todos los demás componentes. Actúa como una plataforma sobre la cual se ensamblan la CPU, la memoria, las tarjetas de expansión y otros dispositivos. Proporciona conexiones físicas y eléctricas, así como también controla la inicialización y configuración del sistema.</p>
-            <div class="des-img">
-                <img src="img/mb.jpg" alt="">
-            </div>
-        </div>
-        <div class="inf-c"
-        <h2 class="inf-c-tl"></h2>
-            <div class="inf-c-list">
-                <li>Placa Base o Motherboard</li>
-                <li>Conectividad para otros componentes.</li>
-                <li>Circuito impreso con pistas eléctricas.</li>
-                <li>Contiene BIOS/UEFI para inicialización.</li>
-                <li>Chipset para control de comunicación.</li>
-                <li>Determina factor de forma y compatibilidad.</li>
-            </div>
-        </div>
-    </div>
-
+    a
 
     `;
