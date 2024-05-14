00d9be5d6c18af1cd6bf347438b90f6b624566c9
adrodas <adrodas@scl.edu.gt>
2024-05-14 11:35:31 -0600

    version_2.0 footer and css-2_

---
+++ js/sectioninf.js
@@ -0,0 +1,34 @@
+let li_section=document.querySelector(".inf")
+    li_section.innerHTML=`
+    <div class="inf-mn">
+        <nav class="menu">
+            <li class="menu-opt-1"><a href="index.html">Index</a></li>
+            <li class="menu-opt-2"><a href="cpu.html">CPU</a></li>
+            <li class="menu-opt-3"><a href="gpu.html">GPU</a></li>
+            <li class="menu-opt-4"><a href="mb.html">MB</a></li>
+            <li class="menu-opt-5"><a href="ram.html">RAM</a></li>
+        </nav>
+    </div>
+    <div class="inf-des">
+        <h1 class="des-tl">CPU</h1>
+        <div class="des-tx">
+            <p> La CPU, o procesador, es el componente esencial de un ordenador que ejecuta instrucciones de programas y realiza operaciones aritméticas y lógicas. Es responsable de coordinar y controlar el funcionamiento del sistema, interpretando y ejecutando comandos para procesar datos.</p>
+            <div class="des-img">
+                <img src="img/cpu.jpg" alt="">
+            </div>
+        </div>
+        <div class="inf-c"
+        <h2 class="inf-c-tl"></h2>
+            <div class="inf-c-list">
+                <li class="l1">Ejecuta instrucciones de programas.</li>
+                <li class="l1">Realiza calculos aritmeticos y logicos.</li>
+                <li class="l1">Coordina el funcionamiento del sistema </li>
+                <li class="l1">Velocidad medida en GHz</li>
+                <li class="l1">Nucleos para multitarea</li>
+                <li class="l1">Componente principal de una computadora</li>
+            </div>
+        </div>
+    </div>
+
+
+    `;
